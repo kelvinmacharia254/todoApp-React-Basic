1849aa64bce8a543bdd6f75812de41625b553e24
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-07-11 12:58:53 +0300

    add edit task functionality using conditional rendering.
    Note:Check/uncheck,delete and editTask works by modyfying tasks global state and ensuring it in sync with UI.
    	 Changes in the tasks state re-renders the entire application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Todo from "./components/Todo.jsx";
 import Form from "./components/Form.jsx";
 import FilterButton from "./components/FilterButton.jsx";
+
+import {useState} from "react";
+// install "npm install nanoid"
+// use to generate unique ids
+import { nanoid } from "nanoid";
+
 function App(props) {
     // const taskList = props.tasks?.map((task) => (
     //     <Todo
@@ -11,9 +17,60 @@ function App(props) {
     //     />
     // ));
     // console.log(taskList);
+
+    const [tasks, setTasks] = useState(props.tasks);
     function addTask(name){
-        name? alert(name):alert("Task empty. Type name of a task to be done.");
+        // Function passed as callback prop to the Form component to fetch task name
+
+        // Test code
+        // name? alert(name):alert("Task empty. Type name of a task to be done.");
+
+        // Receive task from Form component
+        // Update task state
+        // Note: name is a string. Restructure to object to match task object state
+        const newTask = {id:`todo-${nanoid()}`, name, completed: false};
+        // update tasks on top of existing tast
+        setTasks([...tasks, newTask]);
     }
+
+    function toggleTaskCompleted(id){
+        // update task completion status.
+        // update tasks completion status depending on the status of the checkbox.
+        // Note: This step is syncing the app with the UI. Synchronizing the browser with the underlying state data
+        const updatedTasks = tasks.map((task) =>{
+            // if this task has the same ID as the edited task
+            if(task.id === id){
+                return {...task, completed: !task.completed}; // update each task completion by inversion of boolean
+            }
+            return task
+        })
+        setTasks(updatedTasks); // update tasks state
+    }
+
+    function deleteTask(id){
+        // delete task
+         const remainingTasks = tasks.filter((task) => id !== task.id);
+         console.log(remainingTasks);
+         setTasks(remainingTasks);
+    }
+
+    // Count tasks
+    const tasksNoun = tasks.length !== 1? "tasks": "task"
+    const headingText = `${tasks.length} ${tasksNoun}`;
+
+    function editTask(id, newName){
+        // edit task
+
+        const editedTaskList = tasks.map((task) =>{
+            if(task.id === id){
+                return {...task, name: newName};
+            }
+            return task
+        })
+        setTasks(editedTaskList);
+    }
+    // log task
+    console.log(tasks);
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -24,8 +81,7 @@ function App(props) {
             <FilterButton/>
         </div>
 
-
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
@@ -33,12 +89,15 @@ function App(props) {
         {/*<Todo name="Eat" id="todo-0" completed/>*/}
         {/*<Todo name="Sleep" id="todo-1"/>*/}
         {/*<Todo name="Repeat" id="todo-2"/>*/}
-        {props.tasks?.map((task) => (
+        {tasks?.map((task) => (
         <Todo
             id={task.id}
             name={task.name}
             completed={task.completed}
             key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
+            deleteTask={deleteTask}
+            editTask={editTask}
         />
     ))}
       </ul>
